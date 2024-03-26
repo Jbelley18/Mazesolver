f63626d705862815f28c424de8a03abb845157e0
jbelley18 <jbelley@wgu.edu>
2024-03-26 07:00:45 -0400

    bulk of code for the mazesolver

--- graphics.py
+++ graphics.py
@@ -1,5 +1,32 @@
 from tkinter import Tk, BOTH, Canvas
 
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("window closed...")
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+    def close(self):
+        self.__running = False
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -7,43 +34,16 @@ class Point:
 
 
 class Line:
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
+        self.p1 = p1
+        self.p2 = p2
 
-    def draw(self, canvas, fill_color):
-        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
-        canvas.pack()
-
-class Window:
-    def __init__(self, width, height):
-        self.root = Tk()
-        self.root.title("My Window")
-
-        # Create and pack the canvas
-        self.canvas = Canvas(self.root, width=width, height=height)
-        self.canvas.pack()
-
-        # Data member to track the "running" state of the window
-        self.running = False
-
-        # Set the method to call when the window is closed
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        # Redraw the window
-        self.root.update_idletasks()
-        self.root.update()
-
-    def wait_for_close(self):
-        # Run the window until it's closed
-        self.running = True
-        while self.running:
-            self.redraw()
-
-    def close(self):
-        # Close the window
-        self.running = False
-
-    def draw_line(self, line, fill_color):
-        line.draw(self.canvas, fill_color)
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+        canvas.pack(fill=BOTH, expand=1)
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from graphics import Window, Line, Point
+from graphics import Window
+from maze import Maze
+import sys
+
 
 def main():
-    # Example of usage in main function
-    window = Window(800, 600)
-    p1 = Point(100, 100)
-    p2 = Point(200, 200)
-    line = Line(p1, p2)
-    window.draw_line(line, "red")
-    window.wait_for_close()
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
 
-# Make sure to call main() properly
-if __name__ == "__main__":
-    main()
+    sys.setrecursionlimit(10000)
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    print("maze created")
+    is_solveable = maze.solve()
+    if not is_solveable:
+        print("maze can not be solved!")
+    else:
+        print("maze solved!")
+    win.wait_for_close()
+
+
+main()
